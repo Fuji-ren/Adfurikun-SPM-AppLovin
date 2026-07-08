a43298bb71d89ef91b4f2247272348b28902921d
Fuji-ren <renfu.3731@gmail.com>
2026-07-08 16:12:43 +0900

    first

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "Adfurikun-SPM-AppLovin",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "AdfurikunAppLovin", targets: ["AdfurikunAppLovin"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Fuji-ren/Adfurikun-SPM-Core.git",
+            from: "4.4.0.0"
+        ),
+        .package(
+            url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git",
+            exact: "13.5.1"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "AdfurikunAppLovin",
+            dependencies: [
+                .product(name: "AdfurikunSDK", package: "Adfurikun-iOS-SDK"),
+                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package")
+            ],
+            path: "Sources",
+            publicHeadersPath: "."
+        )
+    ]
+)
