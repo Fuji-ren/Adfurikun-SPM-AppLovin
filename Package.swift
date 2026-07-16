d1b39b3a2b97859b0be21b2392b62b6aeb42da7d
Fuji-ren <renfu.3731@gmail.com>
2026-07-16 18:07:15 +0900

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Fuji-ren/Adfurikun-SPM-Core.git",
-            exact: "4.4.2"
+            exact: "4.4.3"
         ),
         .package(
             url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git",
